6a0495fb25ec8994411a673bac860e6f0311c2f2
karsyn ansari <karsyn.ansari@gmail.com>
2022-01-16 10:21:31 -0700

    commit tree 033b4ca57a477616f23a4f6c670e904644672c16

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+// const totalBatteries = (previousValue, currentValue) =>
+//   previousValue + currentValue;
+// batteryBatches.reduce(totalBatteries, 0);
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(function (
+  previousValue,
+  currentValue
+) {
+  return previousValue + currentValue;
+},
+0);
+
+// const doubledAndSummed = [1, 2, 3].reduce(function (accumulator, element) {
+//   return element * 2 + accumulator;
+// }, 0);
